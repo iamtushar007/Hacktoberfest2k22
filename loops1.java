8b8f0ffcb6700a5acdd1cce2af2a5bee1f69b11c
Harshit Saxena <74541424+HarshitSaxena12@users.noreply.github.com>
2022-10-14 00:23:00 +0530

    Add files via upload

---
+++ loops1.java
@@ -0,0 +1,12 @@
+package com.harshit;
+import java.util.Scanner;
+public class loops1{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+            System.out.print( i + "" );
+            System.out.println("tert");
+        }
+    }
+}
